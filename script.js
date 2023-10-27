12c15507bb075ccf32a48eb88706a4d72fb8f29e
Andriy29k <adnriykorotchuk345@gmail.com>
2023-10-27 12:36:54 +0300

    "Stable version 1.0"

--- script.js
+++ script.js
@@ -1,55 +1,63 @@
-// const sumbitButton = document.getElementById("sumbit");
-// const url = "http://tunetc.ddns.net/api/fmi_schedule/3/%D0%86%D0%9F%D0%97-31";
-const specialty_url = "http://tunetc.ddns.net/api/fmi_schedule/all_specialty";
-// async function fetchHandler() {
-//   try {
-//     const response = await fetch(url);
-//     console.log(response);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-// sumbitButton.addEventListener("click", function () {
-//   fetchHandler();
-// });
-// const select = document.getElementById("group");
-// Отримайте посилання на елементи <select>
 const courseSelect = document.getElementById("course");
 const groupSelect = document.getElementById("group");
+const submitButton = document.getElementById("sumbit");
 
-fetch(specialty_url) // Замість "data.json" вкажіть URL вашого JSON-файлу
+// Отримайте посилання на елемент, де ви будете відображати розклад
+const scheduleDisplay = document.getElementById("scheduleDisplay");
+
+let data; // Оголосіть змінну data на рівні модуля
+
+const specialty_url = "http://tunetc.ddns.net/api/fmi_schedule/all_specialty";
+
+fetch(specialty_url)
   .then(response => response.json())
-  .then(data => {
-    // data - це ваш JSON-об'єкт
+  .then(jsonData => {
+    data = jsonData;
 
     // Отримайте ключі JSON-об'єкта (кореспондують курсам) і додайте їх до вибору "course"
     for (const key in data) {
       if (data.hasOwnProperty(key)) {
         const option = document.createElement("option");
         option.value = key;
-        option.textContent = key + " курс"; // Додайте "курс" до назви курсу
+        option.textContent = key + " курс";
         courseSelect.appendChild(option);
       }
     }
-
-    // Обробник події для вибору курсу
-    courseSelect.addEventListener("change", () => {
-      const selectedCourse = courseSelect.value;
-      const groupData = data[selectedCourse];
-
-      // Очистіть вибір групи перед оновленням
-      groupSelect.innerHTML = "";
-
-      // Пройдіться по кожному елементу у масиві "groupData" і додайте їх до вибору "group"
-      groupData.forEach(group => {
-        const option = document.createElement("option");
-        option.value = group.specialty;
-        option.textContent = group.specialty;
-        groupSelect.appendChild(option);
-      });
-    });
 
     // Ініціалізуйте вибір курсу за замовчуванням
     courseSelect.dispatchEvent(new Event("change"));
   })
   .catch(error => console.error("Помилка при отриманні JSON: " + error));
+
+// Оновлений обробник події для вибору курсу
+courseSelect.addEventListener("change", () => {
+  const selectedCourse = courseSelect.value;
+  const groupData = data[selectedCourse];
+  groupSelect.innerHTML = "";
+
+  groupData.forEach(group => {
+    const option = document.createElement("option");
+    option.value = group.specialty;
+    option.textContent = group.specialty;
+    groupSelect.appendChild(option);
+  });
+});
+
+// Обробник події для натискання кнопки "sumbit"
+submitButton.addEventListener("click", () => {
+  const selectedCourse = courseSelect.value;
+  const selectedGroup = groupSelect.value;
+
+  // Створіть URL для запиту до API розкладу
+  const apiUrl = `http://tunetc.ddns.net/api/fmi_schedule/${selectedCourse}/${selectedGroup}`;
+
+  // Виконайте запит до API та відображайте результат у scheduleDisplay
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(scheduleData => {
+      console.log(scheduleData); // Виведіть JSON у консоль
+    })
+    .catch(error => {
+      console.error("Помилка при отриманні розкладу: " + error);
+    });
+});
